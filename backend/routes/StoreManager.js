7bdda53592821bcb58410d89565362ad5e84fc6a
sathiralamal <sdsat756@gmail.com>
2020-05-13 01:18:38 +0530

    add edit storemanager

--- backend/routes/StoreManager.js
+++ backend/routes/StoreManager.js
@@ -56,21 +56,68 @@ router.route('/').get((req,res)=>{
 
 
 
-router.route('/update/:id').put((req,res)=>{
+router.route('/update/:id').post((req,res)=>{
+
+    let selected_id=req.params.id;
+
+    let updatedfirstName=req.body.firstName;
+    let updatedlastName=req.body.lastName;
+    let updatedbirthDay=req.body.birthDay;
+    let updatedpassword =req.body.password;
+    let updatedemailAddress=req.body.email;
+    let updatedaddress=req.body.address;
+    let updatedtelephoneNumber=req.body.telephonenumber;
+    console.log("recive_data: "+updatedfirstName+updatedlastName+updatedbirthDay+updatedpassword+updatedemailAddress+updatedaddress+updatedtelephoneNumber);
+    
+    StoreManager.findByIdAndUpdate(
+         selected_id,
+        { 
+            
+                "firstName": updatedfirstName,
+                "lastName":updatedlastName,
+                "emailAddress":updatedemailAddress,
+                "birthDay":updatedbirthDay,
+                "address":updatedaddress,
+                "telephoneNumber":updatedtelephoneNumber
+                
+        
+        },(err,storemanager)=>{
+           
+            res.send(storemanager);
+           
+        }
+     )
+
+
+    // const updatedStoreManager=new StoreManager({
+    //     firstName,
+    //     lastName,
+    //     password,
+    //     emailAddress,
+    //     birthDay,
+    //     address,
+    //     telephoneNumber
+    // });
       
-    StoreManager.findById(req.params.id)
-        .then(storemanager=>{
-            storemanager.firstName=req.body.firstName;
-            storemanager.lastName=req.body.lastName;
-            storemanager.birthDay=req.body.birthDay;
-            storemanager.emailAddress=req.body.email;
-            storemanager.address=req.body.address;
-            storemanager.telephoneNumber=req.body.telephonenumber;
-            storemanager.save()
-                .then(success=>res.json('storemanager updated'))
-                .catch(err=>res.status(400).json('Error: '+err));
-        })
-        .catch(err=>res.status(400).json('Error: '+err));
+    // StoreManager.findById(req.params.id)
+    //             .then(()=>{
+    //                 updatedStoreManager.save()
+    //                 .then(updatedStoreManager=>res.json('new store manager added'))
+    //                 .catch(err=>res.status(400).json('Error in Create new Store manager '+err));
+    //             })
+
+        // .then(storemanager=>{
+        //     storemanager.firstName=req.body.firstName;
+        //     storemanager.lastName=req.body.lastName;
+        //     storemanager.birthDay=req.body.birthDay;
+        //     storemanager.emailAddress=req.body.email;
+        //     storemanager.address=req.body.address;
+        //     storemanager.telephoneNumber=req.body.telephonenumber;
+        //     storemanager.save()
+        //         .then(success=>res.json('storemanager updated'))
+        //         .catch(err=>res.status(400).json('Error: '+err));
+        // })
+        // .catch(err=>res.status(400).json('Error: '+err));
 
    
 
--- client/src/views/pages/AdminPages/StoreManagerRegister/storemanagertable.js
+++ client/src/views/pages/AdminPages/StoreManagerRegister/storemanagertable.js
@@ -13,20 +13,20 @@ class StoreManager extends Component{
         return(
     <tr >
         <td>
-            <p style={!this.props.editdatastatas?{display:"inherit"}:{display:"none"} }>{this.props.storemanager.firstName} </p>
+            <p style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"none"}:{display:"inherit"} }>{this.props.storemanager.firstName} </p>
              <Input style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"inherit"}:{display:"none"}  }
                                    
                                    placeholder={this.props.storemanager.firstName}
-                                   defaultValue ={this.props.storemanager.firstName}
                                    onChange={(e)=>this.props.handeleEditFirstname(e.target.value)}
-                                   value={this.props.editedfirstame}
+                                   ref={this.props.editedfirstame}
                                    name="firstName"
+                                   
                                   
                                    
              />            
         </td>
         <td>
-            <p style={!this.props.editdatastatas?{display:"inherit"}:{display:"none"} }>{this.props.storemanager.lastName}</p>
+            <p style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"none"}:{display:"inherit"} }>{this.props.storemanager.lastName}</p>
             <Input style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"inherit"}:{display:"none"}  }
                                    placeholder={this.props.storemanager.lastName}
                                    onChange={(e)=>this.props.handeleEditLastname(e.target.value)}
@@ -36,8 +36,8 @@ class StoreManager extends Component{
             
         </td>
         <td>
-           <p style={!this.props.editdatastatas?{display:"inherit"}:{display:"none"} }> {this.props.storemanager.birthDay}</p>
-            <Input style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"inherit"}:{display:"none"}  }
+           <p style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"none"}:{display:"inherit"} }> {this.props.storemanager.birthDay}</p>
+            <Input type="date" style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"inherit"}:{display:"none"}  }
                                    placeholder={this.props.storemanager.birthDay}
                                    onChange={(e)=>this.props.handeleEditBirthday(e.target.value)}
                                    value={this.props.editBirthday}
@@ -46,7 +46,7 @@ class StoreManager extends Component{
              />   
         </td>
         <td>
-           <p style={!this.props.editdatastatas?{display:"inherit"}:{display:"none"} }>{this.props.storemanager.emailAddress}</p> 
+           <p style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"none"}:{display:"inherit"} }>{this.props.storemanager.emailAddress}</p> 
             <Input style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"inherit"}:{display:"none"}  }
                                    placeholder= {this.props.storemanager.emailAddress}
                                    onChange={(e)=>this.props.handeleEmailAddress(e.target.value)}
@@ -56,17 +56,18 @@ class StoreManager extends Component{
             
         </td>
         <td>
-            <p style={!this.props.editdatastatas?{display:"inherit"}:{display:"none"} }>{this.props.storemanager.address}</p>
+            <p style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"none"}:{display:"inherit"} }>{this.props.storemanager.address}</p>
             <Input style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"inherit"}:{display:"none"}  }
                                    placeholder={this.props.storemanager.address}
                                    onChange={(e)=>this.props.handeleEmailAddress(e.target.value)}
                                    value={this.props.editAddress}
                                    name="address"
+
              /> 
         
         </td>
         <td>
-            <p style={!this.props.editdatastatas?{display:"inherit"}:{display:"none"} }>{this.props.storemanager.telephoneNumber}</p>
+            <p style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"none"}:{display:"inherit"} }>{this.props.storemanager.telephoneNumber}</p>
             <Input style={((this.props.editdatastatas)&&(this.props.selectedite_id==this.props.storemanager._id))?{display:"inherit"}:{display:"none"}  }
                                    placeholder={this.props.storemanager.telephoneNumber}
                                    onChange={(e)=>this.props.handeleTelephoneNumber(e.target.value)}
@@ -80,7 +81,15 @@ class StoreManager extends Component{
         </td>
         <td>
         <RiEditLine  size="2em" color="#FFD478" style={!this.props.editdatastatas?{display:"inherit"}:{display:"none"} } onClick={()=>{this.props.editemodeToggle(this.props.storemanager._id)}}  > </RiEditLine>
-        <RiCheckboxCircleLine  size="2em" color="green " style={this.props.editdatastatas?{display:"inherit"}:{display:"none"}  } onClick={()=>{this.props.updateStoremanager()}} ></RiCheckboxCircleLine>
+        <RiCheckboxCircleLine  size="2em" color="#4EB6E6 " style={this.props.editdatastatas?{display:"inherit"}:{display:"none"}  } onClick={()=>{this.props.updateStoremanager(
+            this.props.storemanager.firstName,
+            this.props.storemanager.lastName,
+            this.props.storemanager.birthDay,
+            this.props.storemanager.emailAddress,
+            this.props.storemanager.address,
+            this.props.storemanager.telephoneNumber,
+
+        )}} ></RiCheckboxCircleLine>
         </td> 
     </tr>
         )
@@ -160,8 +169,9 @@ export default class storemanagerview extends Component {
         })
     }   
  
-    handeleEditFirstname=(event)=>{       
+    handeleEditFirstname=(event)=>{                
         this.setState({editFirstname: event})
+                               
     }
 
     handeleEditLastname(event){
@@ -194,13 +204,35 @@ export default class storemanagerview extends Component {
         
     }
 
-    onSubmitUpdateForm(){
+    onSubmitUpdateForm(dfname,dlname,dbday,demil,daddress,dtel){
         // event.preventDefault()
-
+        
+               
         console.log("call update form method");
-        console.log(this.state.editFirstname);
-        console.log(this.state.editLastname);
-        console.log(this.state.editAddress);
+            if(this.state.editFirstname==""){
+               this.state.editFirstname=dfname               
+            }
+
+            if(this.state.editLastname==""){
+                this.state.editLastname=dlname;
+            }
+
+            if(this.state.editBirthday==""){
+                this.state.editBirthday=dbday;
+            }
+
+            if(this.state.editEmailAddress==""){
+                this.state.editEmailAddress=demil
+            }
+
+            if(this.state.editAddress==""){
+                this.state.editAddress=daddress
+            }
+
+            if(this.state.editTelephoneNumber==""){
+                this.state.editTelephoneNumber=dtel
+            }
+
         
         const storeManagerUpdated={
             firstName:this.state.editFirstname,
@@ -211,10 +243,11 @@ export default class storemanagerview extends Component {
             telephonenumber:this.state.editTelephoneNumber
         }
 
-        Axios.put('http://localhost:3001/storeManager/update/'+this.state.edite_Id,storeManagerUpdated)
+        Axios.post('http://localhost:3001/storeManager/update/'+this.state.edite_Id,storeManagerUpdated)
         .then(res=>console.log("store manager update sucessful"+res.data))
         .catch(err=>console.log('error in update :'+err.data))
         this.editmodeToggle();
+        this.loadStoreManagerData();
 
     }
     
@@ -291,7 +324,7 @@ export default class storemanagerview extends Component {
           <Container style={Styles.regTablePlanal}>
                 <h4 style={Styles.regHeadertext}>Register new Store Manager</h4>
                     <Input type="text" onChange={this.handleSearch} placeholder="Search hear"></Input>
-                    <Table  responsive >
+                    <Table  responsive   >
                         <thead>
                               <tr>
             <th>Fname</th>
